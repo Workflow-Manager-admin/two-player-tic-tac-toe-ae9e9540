03d57ca9e901cb369664133eafada1075bd07a36
Kavia AI <production@kavia.ai>
2025-07-09 14:49:34 +0000

    CheckPoint - cme2c452b2

--- .init/.linter.sh
+++ .init/.linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/kavia/workspace/code-generation/two-player-tic-tac-toe-ae9e9540/tic_tac_toe_frontend
+cd /home/kavia/workspace/cme2c452b2/two-player-tic-tac-toe-ae9e9540/tic_tac_toe_frontend
 npm run build
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
